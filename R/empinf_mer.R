ef4764eb9e6ab2857cba2bcf46ff0204c058329e
Mark Lai <marklhc@gmail.com>
2017-08-23 01:40:31 -0400

    Implement empinf_merm() for simutaneously obtaining all influence functions

--- R/empinf_mer.R
+++ R/empinf_mer.R
@@ -6,6 +6,10 @@
 #' \code{empinf_mer} computes non-parametric influence function of models
 #' fitted using \code{\link[lme4]{lmer}} by deleting one cluster at a time. See
 #' van der Leeden, Meijer, and Busing (2008, pp. 420--422) for more information.
+#' Whereas \code{empinf_mer} computes influence values for a specified position
+#' (as specified with the \code{index} argument) of the output of \code{FUN},
+#' \code{empinf_merm} computes influence values for every element in
+#' \code{FUN(x)}.
 #' @param x A fitted merMod object from \code{\link[lme4]{lmer}}.
 #' @param FUN A function taking a fitted merMod object as input and returning
 #'   the statistic of interest, which must be a (possibly named) numeric vector
@@ -24,6 +28,7 @@
 #' # Define function for intraclass correlation
 #' icc <- function(x) 1 / (1 + 1 / getME(x, "theta")^2)
 #' empinf_mer(fm01ML, icc)
+#' empinf_mer(fm01ML, fixef)
 empinf_mer <- function(x, FUN, index = 1) {
   if (length(x@flist) > 1) {
     stop("currently can only compute influence values with two levels")
@@ -48,6 +53,33 @@ empinf_mer <- function(x, FUN, index = 1) {
   th_tilde <- hj * th_n - (hj - 1) * th_noj
   # (hj - 1) * (th_Jmj - th_tilde)
   (hj - 1) * (th_tilde - th_Jmj)
+}
+
+#' @rdname empinf_mer
+empinf_merm <- function(x, FUN) {
+  if (length(x@flist) > 1) {
+    stop("currently can only compute influence values with two levels")
+  }
+  J <- lme4::ngrps(x)[[1]]
+  n <- nobs(x)
+  gp <- x@flist[[1]]
+  nj <- as.vector(unname(table(gp)))
+  hj <- n / nj
+  formula_x <- formula(x)
+  org_data <- x@frame
+  th_n <- FUN(x)
+  th_noj <- matrix(NA, nrow = J, ncol = length(th_n))
+  for (j in seq_len(J)) {
+    i <- which(gp == levels(gp)[j])
+    # m <- lmer(formula_x, data = org_data[-i, ])
+    m_call <- update(x, formula_x, data = org_data[-i, ], evaluate = FALSE)
+    m <- eval(m_call)
+    th_noj[j, ] <- FUN(m)
+  }
+  th_Jmj <- J * th_n - colSums( (1 - 1 / hj) * th_noj)
+  th_tilde <- tcrossprod(hj, th_n) - (hj - 1) * th_noj
+  # (hj - 1) * (th_Jmj - th_tilde)
+  (hj - 1) * t(t(th_tilde) - th_Jmj)
 }
 
 empinf_mer_old <- function(x, FUN) {
